4aa2c57a7bea0eb1888e8b2980f71b4876472d45
JiaG293 <giau32002@gmail.com>
2025-03-18 10:12:07 +0700

    1

--- dev-fe-web/vite.config.ts
+++ dev-fe-web/vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
