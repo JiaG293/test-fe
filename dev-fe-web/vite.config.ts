3f1b703079d2672e05fe3425dcf60a4a8a56770c
JiaG293 <giau32002@gmail.com>
2025-03-18 10:36:00 +0700

    1

--- dev-fe-web/vite.config.ts
+++ dev-fe-web/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
+    allowedHosts: ["devroomx.jiag.id.vn", "roomx.jiag.id.vn"],
   },
   resolve: {
     alias: {
